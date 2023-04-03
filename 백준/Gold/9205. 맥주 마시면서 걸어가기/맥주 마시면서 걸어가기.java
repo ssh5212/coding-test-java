5cff4ad3a4302171e8dbc48d3154913d785c6126
AngelPlayer <ssh5212@naver.com>
2023-04-03 21:44:47 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 280 ms, Memory: 20372 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,83 +1,84 @@
 import java.io.*;
 import java.util.*;
 
+// 경유비를 최소로 하는 문제로 볼 수도 있음
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	static int[][] map;
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		
+
 		int T = Integer.parseInt(st.nextToken());
-		
+
 		for (int t = 0; t < T; t++) {
 			// 첫 번째 줄
 			st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken()); // 편의점 개수
-			
-			int N = n + 2; // 한 줄의 길이
-			String answer = "";
-			
-			
-			int[][] map = new int[n+2][n+2];
-			int INF = 9999999;
-			for (int i = 0; i < map.length; i++) {
-				Arrays.fill(map[i], INF);
-			}
-			
-			int[] inputX = new int[n+2];
-			int[] inputY = new int[n+2];
-			
-			
+			int N = n + 2;
+
+			map = new int[N][N];
+
+			int[] inputX = new int[N]; // 사용자 입력 저장
+			int[] inputY = new int[N];
+
+			int INF = 9999999; // 절대값
+
+			// 나머지 줄
+			// 맨 앞 : 집 | 맨 뒤 : 패스티벌 좌표
 			for (int i = 0; i < N; i++) {
-				// 나머지 줄
 				st = new StringTokenizer(br.readLine());
 				inputX[i] = Integer.parseInt(st.nextToken());
 				inputY[i] = Integer.parseInt(st.nextToken());
 			}
-			
+
+			// 맨해튼 거리 계산
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
+					// 자기 자신까지 거리는 0
 					if (i == j) {
 						map[i][j] = 0;
-					} else {
-						map[i][j] = Math.abs(inputX[i] - inputX[j]) + Math.abs(inputY[i] - inputY[j]);
-						if(map[i][j] > 1000) {
-							map[i][j] = INF;
-						}
+						continue;
+					}
+					map[i][j] = Math.abs(inputX[i] - inputX[j]) + Math.abs(inputY[i] - inputY[j]);
+					if (map[i][j] > 1000) {
+						map[i][j] = INF;
 					}
 				}
 			}
-//			print(map);
-//			System.out.println();
-			
 
-			// 경출도
-			for (int k = 0; k < N; k++) { // 경
-				for (int i = 0; i < N; i++) { // 출
-					if (i == k) continue; // 출발지와 경유지가 같다면 continue
-					for (int j = 0; j < N; j++) { // 도
-						if (i == j || k == j) continue;
-						if (map[i][k] + map[k][j] <= 2000) {
-							map[i][j] = 1000;
+//			print(map);
+
+			// 플로이드 워셜 : 경출도
+			for (int k = 0; k < N; k++) { // 경유지
+				for (int i = 0; i < N; i++) { // 출발지
+					// 경유지와 출발지가 같다면 pass
+					if (k == i)
+						continue;
+					for (int j = 0; j < N; j++) { // 도착지
+						// 출발-경유, 경유-도착이 같다면 pass
+						if (i == k || k == j)
+							continue;
+						// 출발지에서 도착지까지의 거리가, 출발-경유 + 경유-도착보다 오래 걸린다면 갱신
+						if (map[i][j] > map[i][k] + map[k][j]) {
+							// 갱신된 거리가 2000이하라면 (한번에 갈 수 있는 거리라면)
+							if (map[i][k] + map[k][j] <= 2000) {
+								map[i][j] = 1000;
+							}
 						}
 					}
 				}
 			}
-			
+
 //			print(map);
-			if (map[0][N-1] <= 1000) {
-				System.out.println("happy");				
+
+			if (map[0][N - 1] <= 1000) {
+				System.out.println("happy");
 			} else {
-				System.out.println("sad");								
+				System.out.println("sad");
 			}
-			
-			
-			
-			
-			
-			
-			
-			
+
 		}
 	}
 
@@ -88,7 +89,6 @@ public class Main {
 			}
 			System.out.println();
 		}
+
 	}
-	
-	
 }
