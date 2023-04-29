bcd1978087a9537e3b06aa834ab3e837922f2661
AngelPlayer <ssh5212@naver.com>
2023-04-29 09:05:31 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -4,3 +4,9 @@ from ANIMAL_INS I join ANIMAL_OUTS O
     on I.ANIMAL_ID = O.ANIMAL_ID
 where I.DATETIME > O.DATETIME
 order by I.DATETIME asc;
+
+# select ANIMAL_ID, NAME
+# from ANIMAL_INS
+# where DATETIME any (select DATETIME
+#                   from ANIMAL_OUTS)
+# order by DATETIME asc;
