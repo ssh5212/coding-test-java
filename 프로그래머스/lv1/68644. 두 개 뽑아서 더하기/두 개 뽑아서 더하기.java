040636499817baefce479a73ad6408d362934009
AngelPlayer <ssh5212@naver.com>
2023-07-10 17:14:26 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.67 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public Integer[] solution(int[] numbers) {
+        Integer[] answer = {};
+        
+        // set 생성
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+                
+            }
+        }
+        
+        // System.out.println(set);
+        
+        // set -> array
+        answer = set.toArray(new Integer[0]);
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
