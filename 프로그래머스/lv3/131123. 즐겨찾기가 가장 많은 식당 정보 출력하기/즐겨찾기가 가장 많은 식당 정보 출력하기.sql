3a860a5e43adf8fe272fc8b2b7bbde3ae7877e4a
AngelPlayer <ssh5212@naver.com>
2023-07-19 21:41:05 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/lv3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요
-select FOOD_TYPE, REST_ID, REST_NAME, max(FAVORITES) as FAVORITES
+select FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
 from rest_info
-where FOOD_TYPE='일식'  || FOOD_TYPE='중식' || FOOD_TYPE='한식'
-order by FAVORITES desc
-
-# select *
-# from rest_info
+where (FOOD_TYPE, FAVORITES) in 
+    (select FOOD_TYPE, max(FAVORITES)
+    from rest_info
+    group by FOOD_TYPE)
+order by FOOD_TYPE desc
