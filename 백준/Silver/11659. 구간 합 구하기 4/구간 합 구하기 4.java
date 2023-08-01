62187309ef43e749ba464f02f839ca8a94d0dc5b
AngelPlayer <ssh5212@naver.com>
2023-08-01 23:21:34 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1992 ms, Memory: 64844 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,32 +1,36 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+        // 첫 번째 줄
+        st = new StringTokenizer(br.readLine());
 
-		int[] arr = new int[N];
+        int N = Integer.parseInt(st.nextToken()); // 숫자 개수
+        int M = Integer.parseInt(st.nextToken()); // 합을 구해야 하는 횟수
 
-		int[] S = new int[N + 1];
+        int[] s = new int[N + 1];
+        // 두 번째 줄
+        st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
-		}
+        for (int i = 1; i <= N; i++) {
+            s[i] = s[i - 1] + Integer.parseInt(st.nextToken());
+        }
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
+//        System.out.println(Arrays.toString(s));
+        for (int i = 0; i < M; i++) {
+            // 나머지 줄
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-			System.out.println(S[m] - S[n - 1]);
+            System.out.println(s[end] - s[start - 1]);
 
-		}
+        }
 
-	}
-
+    }
 }
