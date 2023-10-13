eab002066ae516a5578cbece671e78f6db72ca39
AngelPlayer <ssh5212@naver.com>
2023-10-13 23:02:28 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, date_format(r.CREATED_DATE, "%Y-%m-%d") as CREATED_DATE
+from USED_GOODS_BOARD b join USED_GOODS_REPLY r
+    on b.BOARD_ID = r.BOARD_ID
+where b.CREATED_DATE like "2022-10-%"
+# group by
+# having
+order by CREATED_DATE asc, b.title asc
