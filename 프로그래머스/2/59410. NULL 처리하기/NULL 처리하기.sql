02a819bcbc5df7a9713ffdf4500192147fdf8ebf
AngelPlayer <ssh5212@naver.com>
2023-10-14 14:16:32 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -0,0 +1,8 @@
+# -- 코드를 입력하세요
+# select ANIMAL_TYPE, ifnull(NAME, "No name") as NAME, SEX_UPON_INTAKE # 동물의 생물 종, 이름, 성별 및 중성화 여부
+# from ANIMAL_INS # null -> No name 표기
+# order by ANIMAL_ID # 아이디 순 조회
+
+select ANIMAL_TYPE, ifnull(NAME, 'No name') as NAME, SEX_UPON_INTAKE
+from ANIMAL_INS 
+order by ANIMAL_ID
