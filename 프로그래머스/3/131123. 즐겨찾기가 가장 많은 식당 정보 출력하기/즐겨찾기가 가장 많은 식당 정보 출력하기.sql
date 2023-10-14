d67bce98b4966c50cefd73b59a0201c71fe688f2
AngelPlayer <ssh5212@naver.com>
2023-10-14 16:19:25 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -0,0 +1,18 @@
+# -- 코드를 입력하세요
+# select FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+# from rest_info
+# where (FOOD_TYPE, FAVORITES) in 
+#     (select FOOD_TYPE, max(FAVORITES)
+#     from rest_info
+#     group by FOOD_TYPE)
+# order by FOOD_TYPE desc
+
+select FOOD_TYPE, REST_ID, REST_NAME, FAVORITES
+from REST_INFO 
+where (FOOD_TYPE, FAVORITES) in
+    (
+        select FOOD_TYPE, max(FAVORITES)
+        from REST_INFO
+        group by FOOD_TYPE
+    )
+order by FOOD_TYPE desc;
