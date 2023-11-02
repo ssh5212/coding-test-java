c92999c411d88d561a2ed3949288054eabca70cc
AngelPlayer <ssh5212@naver.com>
2023-11-02 21:34:43 +0900

    [Silver IV] Title: 수 찾기, Time: 1828 ms, Memory: 83552 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -4,53 +4,48 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken()); // 비교 대상 숫자 개수
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
 		int[] arr = new int[N];
-
+		
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(arr);
-
+		
 		st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken()); // 존재 여부 확인 할 숫자 개수
-
-		int answer;
+		int M = Integer.parseInt(st.nextToken());
+		
+		
 		st = new StringTokenizer(br.readLine());
+		int answer = 0;
+		
 		for (int i = 0; i < M; i++) {
-			int checkNum = Integer.parseInt(st.nextToken());
+			int now = Integer.parseInt(st.nextToken());
 			answer = 0;
-
-			int start = 0; // 배열의 시작 위치
-			int end = arr.length - 1; // 배열의 끝 위치
-
-			while (start <= end) {
-				int mid = (start + end) / 2; // 배열의 중간 위치
-
-				// 찾으려는 값보다 배열에 들어가 있는 값이 더 작다면
-				if (checkNum > arr[mid]) {
-					start = mid + 1;
+			
+			int start = 0;
+			int end = arr.length -1;
+			
+			
+			while(start <= end) {
+				int center = (start + end)/2;
+				if (arr[center] > now) {
+					end = center - 1;
 				}
-
-				// 찾으려는 값보다 배열에 들어가 있는 값이 더 크다면
-				if (checkNum < arr[mid]) {
-					end = mid - 1;
+				if (arr[center] < now) {
+					start = center + 1;
 				}
-
-				// 찾으려는 값과 배열에 들어간 값이 같다면
-				if (checkNum == arr[mid]) {
-					end = arr[mid];
+				if (arr[center] == now) {
 					answer = 1;
 					break;
 				}
-
 			}
 			System.out.println(answer);
-
 		}
-
 	}
+
 }
