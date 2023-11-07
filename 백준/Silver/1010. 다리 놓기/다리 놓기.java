f3dd853e939cc6adac834ff72094ac97f78908c5
AngelPlayer <ssh5212@naver.com>
2023-11-07 20:14:35 +0900

    [Silver V] Title: 다리 놓기, Time: 108 ms, Memory: 12352 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,35 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int[][] dp = new int[30][30];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
 
-		for (int t = 0; t < T; t++) {
-
+		
+		for (int i = 0; i < T; i++) {
 			st = new StringTokenizer(br.readLine());
-			int R = Integer.parseInt(st.nextToken());
 			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 
-			int[][] b = new int[N + 1][R + 1];
-
-			// 파스칼의 삼각형 공식 이용
-			// nCr == nC(n-r)
-			for (int i = 0; i <= N; i++) {
-				// 뽑으려는 R보다 N이 작은 경우 N개만큼 뽑겠다
-				for (int j = 0, end = Math.min(i, R); j <= end; j++) {
-					if (j == 0 || i == j) { // 파스칼 삼각형의 끝 부분
-						b[i][j] = 1; // 1로 채움
-					} else { // 가운데 부분은 두 가지 수의 영향을 받음
-						b[i][j] = b[i - 1][j - 1] + b[i - 1][j];
-					}
-				}
-			}
-			System.out.println(b[N][R]);
-
+			System.out.println(recursive(M, N));
 		}
-
+		
 	}
+	
+	static int recursive(int m, int n) {
+
+		if (dp[m][n] > 0) {
+			return dp[m][n];
+		}
+		if (m == n || n == 0) {
+			return dp[m][n] = 1;
+		}
+		
+		// 조합 공식 (m-1)C(n-1) + (m-1)C(n) 
+		return dp[m][n] = recursive(m - 1, n - 1) + recursive(m - 1, n);
+	}
+
 }
