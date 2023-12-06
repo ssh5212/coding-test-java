40a352d8b09c109d1adcb98c984d8de85d2a127b
AngelPlayer <ssh5212@naver.com>
2023-12-06 22:02:03 +0900

    [level 1] Title: 예산, Time: 0.41 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        // System.out.println(Arrays.toString(d));
+        
+        for (int i = 0; i < d.length; i++) {
+            if (d[i] <= budget) {
+                budget -= d[i];
+                answer++;
+            } else {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
