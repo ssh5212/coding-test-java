2e8ae13e5f3461daf450381f2587b95da6933764
AngelPlayer <ssh5212@naver.com>
2023-12-26 22:59:27 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 92 ms, Memory: 11956 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -1,31 +1,38 @@
 import java.io.*;
 import java.util.*;
 
-// 남규 >= 영훈 + 2
-// 택희 : 택희 % 2 != 0
-// 남는 사탕은 없어야 함
-
-// if 영훈 != 0
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int count = 0;
-
-        for (int i = 0; i <= N; i++) { // 남규
-            for (int j = 0; j <= N; j++) { // 영훈
-                for (int k = 0; k <= N - 1; k++) { // 택희
-                    if ( (i + j + k == N) && (i >= j + 2) && (k % 2 == 0) && (i != 0 && j != 0 && k != 0) ) {
-                        // System.out.println(i + " " + j + " " + k);
-                        count++;
-                    }
-                }
-            }
-        }
-    System.out.println(count);
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		
+		int answer = 0;
+		
+		for (int i = 1; i < 99; i++) { // 남규
+			for (int j = 1; j < 99; j++) { // 영훈
+				for (int k = 1; k < 99; k++) { // 택희
+					// 조건 1
+					if (i + j + k != N) {
+						continue;
+					}
+					// 조건 2
+					if (i < j + 2) {
+						continue;
+					} 
+					// 조건 3
+					if (k % 2 != 0) {
+						continue;
+					}
+					answer++;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
 
 }
