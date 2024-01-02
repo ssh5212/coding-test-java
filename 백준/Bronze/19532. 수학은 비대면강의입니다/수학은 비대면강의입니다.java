dc2b0fa353244552bf0aabc229cd2b73d0759484
AngelPlayer <ssh5212@naver.com>
2024-01-02 11:42:13 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 120 ms, Memory: 12272 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -2,26 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        int A, B, C, D, E, F;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+		
+		int answerX = -1000;
+		int answerY = -1000;
+		
+		for (int i = -999; i <= 999; i++) {
+			for (int j = -999; j <= 999; j++) {
+				if (a * i + b * j == c && d * i + e * j == f) {
+					answerX = i;
+					answerY = j;
+					
+					System.out.println(i + " " + j);
+				}
+			}
+		}
+	}
 
-        A = Integer.parseInt(st.nextToken());
-        B = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        D = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        F = Integer.parseInt(st.nextToken());
-
-        for (int x = -10000; x < 10001; x++) {
-            for (int y = -10000; y < 10001; y++) {
-                if ((A*x + B*y == C) && (D*x + E*y == F)) {
-                    System.out.println(x + " " + y);
-                }
-            }
-        }
-
-    }
 }
